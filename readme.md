ac6dbeecc937e2170e8079d6d6931a1ef5de2023
Lorenzo Cao <lorenzocao25@gmail.com>
2023-05-16 00:36:02 -0700

    Support post request for SET

--- readme.md
+++ readme.md
@@ -6,54 +6,62 @@ This is a still work-in-progress project and is not meant to be used in producti
 There is also the [Experimental Version](https://github.com/lorenzoc25/rudis/tree/experimental) which adapatively uses `io_uring` on supported kernels. If kernel does not support `io_uring`, it will fall back to using `epoll/kqueue`. This version is not published to the crates and therefore needs to be cloned and built locally. It requires **nightly rust compiler** to be built.
 
 ## Installation
-```
+```sh
 cargo install rudis-http
 ```
 ## Usage
 To run the server, simply run
-```
+```sh
 $ rudis-http
 ```
 or you can optionally specify the address, num shards (more to be added later)
-```
+```sh
 $ rudis-http -n <num_shards> -a <address_to_listen_on>
 
-// to view all commands, do
+# to view all commands, do
 $ rudis-http --help
 ```
 And the server will be listening on the port you specified or `127.0.0.1:6379` by default.
 
-Once the server isup and running, its service can be accessed via http request. The following are the supported requests:
+Once the server is up and running, its service can be accessed via http GET request. The following are the supported requests:
 ```
-Get: <your-url>/GET/<key>
-Set: <your-url>/SET/<key>/<value>
+GET: <your-url>/GET/<key> 
+SET: <your-url>/SET/<key>/<value>
 ```
 The response will be in json format. For a SET, you will be getting the status of this command like
-```
+```sh
 $ curl 'localhost:6379/set/hello/world'
 {"SET": "OK"}
 
-// or if SET's arguments are not correct
+# or if SET's arguments are not correct
 $ curl 'localhost:6379/set/hello'
 {"SET": "Invalid"}
 ```
-and for get, you will be getting the key value pair if there's a match, or an empty json object if there's no match
+You can also send a POST request with json as key value pair to support a SET command. (Only single kv pair is supported for now, and if there's more than one kv pair, only the first one will be used)
+```sh
+$ curl -X POST 'localhost:6379/set' -d '{"hello":"world"}'
+{"SET": "OK"}
+
+$ curl -X POST 'localhost:6379/set' -d '{}'
+{"SET": "Invalidc"}
 ```
+and for get, you will be getting the key value pair if there's a match, or an empty json object if there's no match
+```sh
 $ curl 'localhost:6379/get/hello'     
 {"hello":"world"}
 
-// if key does not exist
+# if key does not exist
 $ curl 'localhost:6379/GET/123'
 {}
 
-// if GET's arguments are not correct
+# if GET's arguments are not correct
 $ curl 'localhost:6379/GET'
 {"GET": "Invalid"}
 ```
 Any other types of commands will be responded with an empty json object
 
 ## Todo
-- support post request with json as arguments
+- support post request with json as arguments (multiple kv pairs)
 - more cli options
 - set argument's TTL
 - general purpose client, and build http interface on top of that
@@ -61,3 +69,4 @@ Any other types of commands will be responded with an empty json object
 - authentication
 ### Finsihed Todos
 - cachepadded for mutex
+- support post request with json as arguments (single kv)
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -3,7 +3,8 @@ mod get;
 pub use get::Get;
 
 mod set;
-use httparse::{Request, Status};
+use httparse::Request;
+use serde_json::{Result, Value};
 pub use set::Set;
 
 #[derive(Debug)]
@@ -13,6 +14,25 @@ pub enum Command {
     Invalid,
 }
 
+#[derive(Debug)]
+struct Args {
+    valid: bool,
+    command: String,
+    key: String,
+    val: Option<String>,
+}
+
+impl Args {
+    pub fn new_invalid(command_type: &str) -> Self {
+        Args {
+            valid: false,
+            command: String::from(command_type),
+            key: String::from(""),
+            val: None,
+        }
+    }
+}
+
 impl Command {
     pub fn from_bytes(http_request_buff: &[u8]) -> Command {
         if http_request_buff.len() == 0 {
@@ -20,37 +40,94 @@ impl Command {
         }
 
         let mut headers = [httparse::EMPTY_HEADER; 16];
-        let req = parse_request(&http_request_buff, &mut headers).unwrap();
+        let mut req = Request::new(&mut headers);
+        let result = req.parse(http_request_buff).unwrap();
+        let n = result.unwrap();
 
-        let args = split_on_path(req.path.unwrap());
+        let arg = make_args(&req, http_request_buff, n);
 
-        match args[0].to_uppercase().as_str() {
+        match arg.command.as_str() {
             "GET" => {
-                if args.len() < 2 || args[1].is_empty() || args.len() > 2 {
+                if arg.valid == false {
                     return Command::Get(Get::new_invalid());
                 }
-                Command::Get(Get::from_key(args[1]))
+                Command::Get(Get::from_key(arg.key))
             }
             "SET" => {
-                if args.len() < 3 || args[1].is_empty() || args[2].is_empty() || args.len() > 3 {
+                if arg.valid == false {
                     return Command::Set(Set::new_invalid());
                 }
-                Command::Set(Set::from_key_val(args[1], args[2]))
+                Command::Set(Set::from_key_val(arg.key, arg.val.unwrap()))
             }
             _ => Command::Invalid,
         }
     }
 }
 
-fn parse_request<'a>(
-    buff: &'a [u8],
-    headers: &'a mut [httparse::Header<'a>; 16],
-) -> Option<Request<'a, 'a>> {
-    let mut req = Request::new(headers);
-    match req.parse(buff) {
-        Ok(Status::Complete(_)) => Some(req),
-        _ => None,
+fn make_args(req: &Request, request_buff: &[u8], idx_of_body: usize) -> Args {
+    let method = req.method.unwrap();
+    let all_path_vec = split_on_path(req.path.unwrap());
+
+    // using regular GET request (pass data through URL)
+    if method == "GET" {
+        let action = all_path_vec[0];
+        match action.to_uppercase().as_str() {
+            "GET" => {
+                if all_path_vec.len() < 2 || all_path_vec[1].is_empty() || all_path_vec.len() > 2 {
+                    return Args::new_invalid("GET");
+                }
+                let key = all_path_vec[1];
+                return Args {
+                    valid: true,
+                    command: String::from("GET"),
+                    key: String::from(key),
+                    val: None,
+                };
+            }
+            "SET" => {
+                if all_path_vec.len() < 3
+                    || all_path_vec[1].is_empty()
+                    || all_path_vec[2].is_empty()
+                    || all_path_vec.len() > 3
+                {
+                    return Args::new_invalid("SET");
+                }
+                let key = all_path_vec[1];
+                let val = all_path_vec[2];
+                return Args {
+                    valid: true,
+                    command: String::from("SET"),
+                    key: String::from(key),
+                    val: Some(String::from(val)),
+                };
+            }
+            _ => return Args::new_invalid("INVALID"),
+        }
+    } else if method == "POST" {
+        // using POST request (SET ONLY) that passes kv-pair through body
+        if all_path_vec.len() != 1 {
+            return Args::new_invalid("SET");
+        }
+        let body = request_buff[idx_of_body..].to_vec();
+        match parse_json(&body) {
+            Ok(value) => {
+                if let Some(obj) = value.as_object() {
+                    // if multiple kv pairs are passed, only the first one is used  for now
+                    if let Some((key, value)) = obj.iter().next() {
+                        return Args {
+                            valid: true,
+                            command: String::from("SET"),
+                            key: key.to_string(),
+                            val: Some(value.as_str().unwrap().to_string()),
+                        };
+                    }
+                }
+                return Args::new_invalid("SET");
+            }
+            Err(_) => return Args::new_invalid("SET"),
+        }
     }
+    Args::new_invalid("INVALID")
 }
 
 fn split_on_path(input: &str) -> Vec<&str> {
@@ -59,4 +136,9 @@ fn split_on_path(input: &str) -> Vec<&str> {
         return rest.to_vec();
     }
     all
+}
+
+fn parse_json(bytes: &[u8]) -> Result<Value> {
+    let value = serde_json::from_slice(bytes)?;
+    Ok(value)
 }
